f2d1a419fa5238e1a131ad9b20647785a6c99488
Hyan <58123406+hya7745@users.noreply.github.com>
2023-03-03 22:07:34 +0000

    Refiz toda uma bagunça de if else pra algo mais simples.
    Preciso refazer depois da maneira que eu estava tentando, pelo menos aprendi OR ||
    
    nunca tinha passado pela minha cabeça usar algo assim

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
+// aqui o jogador escolhe a opção e o puter escolhe aleatoriamente entre 1 a 3
 let jogador = prompt("Digite uma das opções (Pedra, Papel, Tesoura): ");
 let puter = parseInt((Math.random() * 3) + 1);
 
 
-
-switch(puter) {
-    case 1: puter = "Pedra"; break;
-    case 2: puter = "Papel"; break;
-    case 3: puter = "Tesoura"; break;
-    default: alert("Valor desconhecido");
+//convertemos o numero escolhido pelo puter em uma opção
+switch (puter) {
+  case 1: puter = "Pedra"; break;
+  case 2: puter = "Papel"; break;
+  case 3: puter = "Tesoura"; break;
+  default: alert("Valor desconhecido");
 }
-
-
-if(puter === jogador){
-  alert("Empate")
-} else if (puter === "Pedra") && (jogador === "Tesoura"){
-    alert("A máquina ganhou!")
-}
-
-
-/* if(player === computer){
-            result.textContent = 'Tie'
-        }
-        else if(player == 'rock'){
-            if(computer == 'paper'){
-                result.textContent = 'Computer Won';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
+//ja eliminamos uma opção aqui, se o que o jogador escolher for igual ao computador, é empate
+if (puter === jogador) {
+  alert("Empate");
   
-            }else{
-                result.textContent = 'Player Won'
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
+  // aqui eu faço varias comparações para determinar se o jogador ganhou, se não ganhou ele avança pro próximo. 
+  //se ele chegar no final e não determinar que o jogador ganhou é vitória do puter.
+
+
+  //inicialmente eu tava tentando fazer um monte de if else, mas achei essa maneira mais efetiva. 
+
+
+  
+  //Com o if else FUNCIONA porém eu não tava conseguindo fazer o código parar após ele chegar em um resultado, não sei se foi problema de escopo, mas tentei usar o return p ele parar de rodar o código assim que achar um resultado mas não estava usando certo.
+//depois preciso aprender escopos dnv!!!
+  
+} else if (jogador === "Pedra" && puter === "Tesoura" ||
+           jogador === "Papel" && puter === "Pedra" ||
+           jogador === "Tesoura" && puter === "Papel") {
+             alert (jogador + " VS " + puter);
+             alert('VITÓRIA DO JOGADOR!');
+             } else {
+             alert(jogador + " VS " + puter);
+             alert('VITÓRIA DO COMPUTADOR!')
+             } 
+  
+  
